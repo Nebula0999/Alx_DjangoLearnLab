bd54d7c779e72af2b8cb5375cea9f298746d49de
Allan <nabuleallan1@gmail.com>
2024-11-08 13:19:28 +0300

    added new files

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('books/', list_books, name='list_books'),  # URL for the function-based view
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # URL for the class-based view
 ]
