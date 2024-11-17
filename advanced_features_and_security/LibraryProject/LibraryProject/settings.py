e39502c6fc01dcea474afc801c408149d4c4b459
Allan <nabuleallan1@gmail.com>
2024-11-17 14:13:32 +0300

    made changes

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -122,4 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'relationship_app.models.User'
+AUTH_USER_MODEL = 'relationship_app.models.User', 'bookshelf.CustomUser'
