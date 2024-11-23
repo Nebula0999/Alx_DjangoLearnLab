0771ec9c7accbe06ff06dff26ff2fa62857aa038
Allan <nabuleallan1@gmail.com>
2024-11-23 14:52:39 +0300

    added new files

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,10 +1,11 @@
 #from django.db import router
-from django.urls import path
+from django.urls import path, include
 from .views import BookList, BookViewSet
 from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
+    path('', include('api.urls'))
 ]
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
