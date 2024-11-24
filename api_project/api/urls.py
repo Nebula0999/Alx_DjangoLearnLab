3006db79e9332de19147f18ef755fd6379154704
Allan <nabuleallan1@gmail.com>
2024-11-24 12:14:47 +0300

    made changes

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -5,7 +5,7 @@ from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
-    path('', include('api.urls'))
+    path('', include('router.urls'))
 ]
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
