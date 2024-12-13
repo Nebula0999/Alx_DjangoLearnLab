948d5bd1c274f8131b4151c052e7a520ff6daa04
Allan <nabuleallan1@gmail.com>
2024-12-13 16:21:53 +0300

    added social media api django project

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
 from .models import CustomUser
 from django.contrib.auth.password_validation import validate_password
+from rest_framework.authtoken.models import Token
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True)
+    def create(self, validated_data):
+        user = super().create(validated_data)
+        Token.objects.create(user=user)  # Create a token for the user
+        return user
 
     class Meta:
         model = CustomUser
