546234fb9d381b33e8fa86533877b948342eac3e
Allan <nabuleallan1@gmail.com>
2024-12-14 17:12:25 +0300

    made changes in accunts views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -4,17 +4,19 @@ from rest_framework.authtoken.models import Token
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from .serializers import RegisterSerializer, UserSerializer
+from rest_framework import permissions
 from .models import CustomUser
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
+    user = CustomUser.objects.all()
 
     def create(self, request, *args, **kwargs):
         response = super().create(request, *args, **kwargs)
         token, created = Token.objects.get_or_create(user=self.get_object())
         return Response({'token': token.key}, status=status.HTTP_201_CREATED)
 
-class LoginView(APIView):
+class LoginView(generics.GenericAPIView):
     def post(self, request, *args, **kwargs):
         from django.contrib.auth import authenticate
         user = authenticate(username=request.data.get('username'), password=request.data.get('password'))
@@ -27,11 +29,13 @@ class ProfileView(generics.RetrieveUpdateAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = UserSerializer
 
+
     def get_object(self):
         return self.request.user
+    
 
 class FollowUserView(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
         try:
@@ -59,3 +63,4 @@ class UnfollowUserView(APIView):
 
         request.user.following.remove(target_user)
         return Response({'message': f'You have unfollowed {target_user.username}'}, status=status.HTTP_200_OK)
+    
